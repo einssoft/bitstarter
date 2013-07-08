efb5dd095c38a23920629bf7d892ca9f680658da
Jens Dietrich <Jens.Dietrich@einssoft.com>
2013-07-08 08:01:40 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('./index.html');
-  response.send(data);
+  var data = fs.readFileSync('./index.html','utf-8');
+  response.send(data.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
